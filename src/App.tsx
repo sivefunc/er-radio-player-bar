84c67f3e37297f21b492b7969de977cfc6755ffe
sivefunc <sivefunc@tuta.io>
2025-10-13 15:53:47 -0400

    refactor: split into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,46 @@
+function TrackPlaying() {
+  return (
+    <div className="bg-red-500 rounded-lg p-1 flex flex-row gap-x-2 w-72">
+      <div className="bg-green-500 w-16 h-full rounded-lg">
+      </div>
+      <div>
+        <div className="text-xs">Now Playing:</div>
+        <div className="text-xs">Track Name</div>
+        <div className="text-xs">Track Author</div>
+      </div>
+    </div>
+  )
+}
+
+function CentralControl() {
+  return (
+    <div className="flex flex-row gap-x-4 items-center">
+      <div className="bg-red-500 w-12 h-12 rounded-full">
+      </div>
+      <div className="bg-red-500 w-16 h-16 rounded-full">
+      </div>
+    </div>
+  )
+}
+
+function RightControl() {
+  return (
+    <div className="flex flex-row gap-x-4 items-center">
+      <div className="bg-red-500 w-16 h-16 rounded-full">
+      </div>
+      <div className="bg-red-500 rounded-full py-2 px-4">
+        Station XYZ
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-[#00080b] h-20 border-t-4 border-neutral-700 flex flex-row justify-between px-3 py-1.5">
-      <div className="bg-red-500 rounded-lg p-1 flex flex-row gap-x-2 w-72">
-        <div className="bg-green-500 w-16 h-full rounded-lg">
-        </div>
-        <div>
-          <div className="text-xs">Now Playing:</div>
-          <div className="text-xs">Track Name</div>
-          <div className="text-xs">Track Author</div>
-        </div>
-      </div>
-      <div className="flex flex-row gap-x-4 items-center">
-        <div className="bg-red-500 w-12 h-12 rounded-full">
-        </div>
-        <div className="bg-red-500 w-16 h-16 rounded-full">
-        </div>
-      </div>
-      <div className="flex flex-row gap-x-4 items-center">
-        <div className="bg-red-500 w-16 h-16 rounded-full">
-        </div>
-        <div className="bg-red-500 rounded-full py-2 px-4">
-          Station XYZ
-        </div>
-      </div>
+      <TrackPlaying />
+      <CentralControl />
+      <RightControl />
     </div>
   )
 }
